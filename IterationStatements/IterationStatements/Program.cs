29c1946697542fee9f5a46d141e2b30867950c9d
Tadiya Brown <tadiyabrown2000@gmail.com>
2022-08-12 13:32:09 -0400

    initial commit

---
+++ IterationStatements/IterationStatements/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace IterationStatements
+{
+    class Program
+    {
+        private static object i;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 5; i++) ;
+
+            Console.WriteLine($"{i}");
+
+        }
+        static void Exercise(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 5; i++) ;
+
+            Console.WriteLine($"{i}");
+        }
+
+        static void Ex(string [] args)
+        {
+            Console.WriteLine("Hello World");
+            for (int i = 0; i < 5; i++) ;
+
+            Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++) ;
+            Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++) ;
+            Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++) ;
+            Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++) ;
+            Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++) ;
+            Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++) ;
+            Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++) ;
+            Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++) ;
+            Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++) ;
+            Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++) ;
+            Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++) ;
+            Console.WriteLine($"{i}");
+
+            for (int i = 0; i < 5; i++) ;
+            Console.WriteLine($"{i}");
+
+            // For five times, check to see if "i" is still less than 5, after adding a one.
+        }
+    }
+}
+
